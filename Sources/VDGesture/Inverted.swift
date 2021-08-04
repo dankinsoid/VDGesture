0a5f164ccd9d719fe46477a6a540cb04cadd3bc0
dankinsoid <30962149+dankinsoid@users.noreply.github.com>
2021-08-04 19:57:39 +0300

    0.5.0

--- Sources/VDGesture/Inverted.swift
+++ Sources/VDGesture/Inverted.swift
@@ -7,46 +7,35 @@
 
 import Foundation
 
-//extension GestureType {
-//
-//    public func inverted() -> Gestures.Inverted<Self> {
-//        Gestures.Inverted(self)
-//    }
-//}
-//
-//extension Gestures {
-//
-//    public struct Inverted<Wrapped: GestureType>: GestureType {
-//        public var wrapped: Wrapped
-//        public var config: GestureConfig { wrapped.config }
-//        public var initialState: State { State(wrapped: wrapped.initialState) }
-//
-//        public init(_ wrapped: Wrapped) {
-//            self.wrapped = wrapped
-//        }
-//
-//        public func recognize(gesture: GestureContext, state: inout State) -> GestureState {
-//            switch wrapped.recognize(gesture: gesture, state: &state.wrapped) {
-//            case .failed:
-//                if state.didStart {
-//                    gesture.debugFail(of: Self.self, reason: "Did start")
-//                    return .failed
-//                } else {
-//                    return .finished
-//                }
-//            case .finished:
-//                return .failed
-//            case .valid:
-//                state.didStart = true
-//                return .failed
-//            case .none:
-//                return .valid
-//            }
-//        }
-//
-//        public struct State {
-//            public var wrapped: Wrapped.State
-//            public var didStart = false
-//        }
-//    }
-//}
+extension GestureType {
+
+    public func inverted() -> Gestures.Inverted<Self> {
+        Gestures.Inverted(self)
+    }
+}
+
+public prefix func !<T: GestureType>(_ rhs: T) -> Gestures.Inverted<T> {
+    Gestures.Inverted(rhs)
+}
+
+extension Gestures {
+
+    public struct Inverted<Wrapped: GestureType>: GestureType {
+        public var wrapped: Wrapped
+        public var config: GestureConfig { wrapped.config }
+        public var initialState: Wrapped.State { wrapped.initialState }
+
+        public init(_ wrapped: Wrapped) {
+            self.wrapped = wrapped
+        }
+
+        public func recognize(context: GestureContext, state: inout Wrapped.State) -> GestureState {
+            switch wrapped.recognize(context: context, state: &state) {
+            case .failed: return .finished
+            case .finished: return .failed
+            case .valid: return .none
+            case .none: return .valid
+            }
+        }
+    }
+}
